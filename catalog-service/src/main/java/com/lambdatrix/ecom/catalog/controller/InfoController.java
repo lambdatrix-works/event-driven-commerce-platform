7215049fa585e82f7971dd96800ef7f211ccdb19
lambdatrix-works <lambdatrix@gmail.com>
2026-01-12 00:28:45 +0530

    Phase 1 Infra Setup : Redis (#2)
    Redis Setup in Catalog Service

--- catalog-service/src/main/java/com/lambdatrix/ecom/catalog/controller/InfoController.java
+++ catalog-service/src/main/java/com/lambdatrix/ecom/catalog/controller/InfoController.java
@@ -1,7 +1,15 @@
 package com.lambdatrix.ecom.catalog.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +22,24 @@ public class InfoController {
   @Value("${spring.application.name}")
   private String serviceName;
 
+  @Autowired RedisTemplate<String, Object> redisTemplate;
+
   @GetMapping("/info")
-  public BuildInfo info() {
-    return new BuildInfo(serviceName, "0.0.1-SNAPSHOT");
+  public ResponseEntity<BuildInfo> info() {
+    var info = new BuildInfo(serviceName, "0.0.1-SNAPSHOT");
+    return ResponseEntity.ok(info);
+  }
+
+  @PostMapping("/info-cache")
+  public ResponseEntity<Void> cacheInfo(@RequestBody BuildInfo info) throws URISyntaxException {
+    redisTemplate.opsForValue().set(serviceName, info);
+    URI uri = new URI("http://localhost:5540");
+    return ResponseEntity.created(uri).build();
+  }
+
+  @GetMapping("/info-cache")
+  public ResponseEntity<BuildInfo> cacheInfoGet() throws URISyntaxException {
+    BuildInfo info = (BuildInfo) redisTemplate.opsForValue().get(serviceName);
+    return ResponseEntity.ok(info);
   }
 }
