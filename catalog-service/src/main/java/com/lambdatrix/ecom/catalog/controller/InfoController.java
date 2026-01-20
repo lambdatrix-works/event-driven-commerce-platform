0587590b31af397a560b910fea33a22817a47c72
lambdatrix-works <lambdatrix@gmail.com>
2026-01-20 12:51:49 +0530

    Phase 1 Infra Setup : Kafka (#3)
    Kafka setup in Catalog Service with base config

--- catalog-service/src/main/java/com/lambdatrix/ecom/catalog/controller/InfoController.java
+++ catalog-service/src/main/java/com/lambdatrix/ecom/catalog/controller/InfoController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +24,12 @@ public class InfoController {
   @Value("${spring.application.name}")
   private String serviceName;
 
+  @Value("${kafka.topic.name}")
+  private String topicName;
+
   @Autowired RedisTemplate<String, Object> redisTemplate;
+
+  @Autowired KafkaTemplate<String, Object> kafkaTemplate;
 
   @GetMapping("/info")
   public ResponseEntity<BuildInfo> info() {
@@ -30,16 +37,27 @@ public class InfoController {
     return ResponseEntity.ok(info);
   }
 
-  @PostMapping("/info-cache")
+  @PostMapping("/cache-info")
   public ResponseEntity<Void> cacheInfo(@RequestBody BuildInfo info) throws URISyntaxException {
     redisTemplate.opsForValue().set(serviceName, info);
     URI uri = new URI("http://localhost:5540");
     return ResponseEntity.created(uri).build();
   }
 
-  @GetMapping("/info-cache")
+  @GetMapping("/cache-info")
   public ResponseEntity<BuildInfo> cacheInfoGet() throws URISyntaxException {
     BuildInfo info = (BuildInfo) redisTemplate.opsForValue().get(serviceName);
     return ResponseEntity.ok(info);
+  }
+
+  @PostMapping("/kafka-info")
+  public ResponseEntity<Void> kafkaInfo(@RequestBody BuildInfo info) throws URISyntaxException {
+    kafkaTemplate.send(topicName, info);
+    return ResponseEntity.accepted().build();
+  }
+
+  @KafkaListener(topics = "${kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+  public void onMessage(BuildInfo info) {
+    System.out.println("Consumed: " + info);
   }
 }
